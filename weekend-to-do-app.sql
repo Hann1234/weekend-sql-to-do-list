ef12a081354c5ddb2055f800002c353c5b5d2322
Hann1234 <christopherjhannigan@gmail.com>
2021-07-03 18:09:15 -0500

    front end index.html completed

--- weekend-to-do-app.sql
+++ weekend-to-do-app.sql
@@ -7,6 +7,11 @@ CREATE TABLE list (
 "completed" varchar(1)
 );
 
+-- Insert test tasks to populate on pageload --
+INSERT INTO list ("task", "notes", "completed")
+VALUES ('Take out trash.', 'Do this before it gets stinky!', 'Y'),
+('Walk doggo.', 'Do not forget to pick up his poo!', 'N');
+
 -- run this statement to view list table
 SELECT *
 FROM list;

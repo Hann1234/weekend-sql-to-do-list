607dce8d5a75cfe7ec9bd169def2ae27b185528b
Hann1234 <christopherjhannigan@gmail.com>
2021-07-03 21:02:18 -0500

    added GET to router.js

--- server/routes/router.js
+++ server/routes/router.js
@@ -2,3 +2,19 @@ const pool = require('../modules/pool');
 const express = require('express');
 const { Pool } = require('pg');
 const router = express.Router();
+
+// GET TABLE INFO
+
+router.get('/', (req, res) => {
+    //create variable to hold SQL query
+    let queryText = 'SELECT * FROM "list";';
+    //make SQL query to DB
+    pool.query(queryText)
+    .then(result => {
+        res.send(result.rows);
+    })
+    .catch(error => {
+        console.log('Error trying to get the koala from Postgres', error);
+        res.sendStatus(500);
+    });
+});

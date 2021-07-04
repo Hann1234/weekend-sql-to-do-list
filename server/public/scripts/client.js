0bdd5e2914f674256b9be6e1b70318bc3a4f164c
Hann1234 <christopherjhannigan@gmail.com>
2021-07-03 22:49:10 -0500

    completed GET-SELECT to get task list from database and append into DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,10 +6,58 @@ function clearInputs(){ //clears inputs
     $('#notesIn').val('');
   }
 
+function getList() { //makes an ajax call to server to get the list
+    console.log('in getList');
+    $('#viewTasks').empty();
+    $.ajax({
+        type: 'GET',
+        url: '/tasks',
+      }).then(function (response){
+        console.log('in getList', response);
+        //append the DOM to include list data
+        for (const task of response) {
+            if(`${task.completed}` == `N`){
+                $('#viewTasks').append(`
+                    <tr>
+                        <td>${task.name}</td>
+                        <td>${task.notes}</td>
+                        <td>${task.completed}</td>
+                        <td>
+                            <button id="completeButton" data-id="${task.id}">Complete</button>
+                        </td>
+                        <td>
+                            <button id="deleteButton" data-id="${task.id}" data-name="${task.name}">Delete</button>
+                        </td>
+                    </tr>
+                `)
+            }else if(`${task.completed}` == `Y`){
+                $('#viewTasks').append(`
+                    <tr>
+                        <td>${task.name}</td>
+                        <td>${task.notes}</td>
+                        <td>${task.completed}</td>
+                        <td>Completed!</td>
+                        <td>
+                             <button id="deleteButton" data-id="${task.id}" data-name="${task.name}">Delete</button>
+                        </td>
+                    </tr>
+                `)
+            }
+        }
+      }).catch(error => {
+      console.log('Error rendering Koala info to DOM', error);
+      })
+}
+
   function onReady() { //runs when page loads and sets up listeners for clicks
     console.log('JQ');
     clearInputs();
- 
+
+    //click listener for adding task goes here
+
+    getList();
+
+    
   }
   
 

2016259d0dc31864d4c396e2cd402d94ed1433dd
Hann1234 <christopherjhannigan@gmail.com>
2021-07-03 20:54:40 -0500

    added pool.js file

---
+++ server/routes/router.js
@@ -0,0 +1,4 @@
+const pool = require('../modules/pool');
+const express = require('express');
+const { Pool } = require('pg');
+const router = express.Router();
--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 5000;
+const port = 5000;
 const router = require('./routes/router')
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('server/public'));
+app.use(express.static('./server/public'));
+// app.use(express.urlencoded({ extended: true })); <-- Used to test initial server setup
+// app.use(express.json());
 
 // ROUTES
 app.use('/tasks', router)
 
 // Start listening for requests on a specific port
-app.listen(PORT, () => {
-  console.log('listening on port', PORT);
+app.listen(port, () => {
+  console.log('listening on port', port);
 });

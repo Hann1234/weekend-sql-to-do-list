65f387f29e32f36a071e626b79acd10817ecb56c
Hann1234 <christopherjhannigan@gmail.com>
2021-07-06 11:57:58 -0500

    updated typo in error message

--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -8,13 +8,14 @@ const taskRouter = express.Router();
 taskRouter.get('/', (req, res) => {
     //create variable to hold SQL query
     let queryText = 'SELECT * FROM "list" ORDER BY "id" DESC;';
+    //'SELECT * FROM "list" ORDER BY “completed” DESC, "id" DESC;';
     //make SQL query to DB
     pool.query(queryText)
     .then(result => {
         res.send(result.rows);
     })
     .catch(error => {
-        console.log('Error trying to get the koala from Postgres', error);
+        console.log('Error trying to get the task from Postgres', error);
         res.sendStatus(500);
     });
 });
